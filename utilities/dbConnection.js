35b0eb8be50d4d41f15b86ad46b7f925a2a154dc
abhishek <abhi.india.us.japan@gmail.com>
2022-12-30 23:25:11 +0530

    -

--- utilities/dbConnection.js
+++ utilities/dbConnection.js
@@ -5,7 +5,7 @@ let db_config = {
     database : process.env.MYSQL_DBNAME,
     user     : process.env.MYSQL_USERNAME,
     password : process.env.MYSQL_PASSWORD,
-    port     : 6963,
+    port     : process.env.MYSQL_PORT,
     connectionLimit : 1000,
     connectTimeout  : 60 * 60 * 1000,
     acquireTimeout  : 60 * 60 * 1000,
@@ -13,7 +13,7 @@ let db_config = {
     charset: 'utf8mb4'
 };
 
-console.log(process.env.MYSQL_HOSTNAME)
+console.log(process.env.MYSQL_HOSTNAME, process.env.MYSQL_PORT)
 var pool  = mysql.createPool(db_config);
 
 module.exports = pool;
